de92916dbc76a94840682a553dc3571722e393cf
Sass <dudu_sass12@hotmail.com>
2022-09-30 08:53:13 -0300

    lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,9 @@
-def study_schedule(permanence_period, target_time):
+def study_schedule(permanence, target_time):
     try:
         counter = 0
-        for i in range(0, len(permanence_period)):
-            if permanence_period[i][0] <= target_time <= permanence_period[i][1]:
+        for i in range(0, len(permanence)):
+            if permanence[i][0] <= target_time <= permanence[i][1]:
                 counter += 1
         return counter
     except TypeError:
         return None
-
-
-
