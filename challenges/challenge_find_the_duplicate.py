ebf91857c47228bba4dd5a70865174ca68faaff5
Sass <dudu_sass12@hotmail.com>
2022-09-30 08:41:53 -0300

    feat:duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    if len(nums) <= 1 or type(nums) != list:
+        return False
+    nums.sort()
+    for n in range(0, len(nums) - 1):
+        if type(nums[n]) is not int or nums[n] < 0:
+            return False
+        if nums[n] == nums[n + 1]:
+            return nums[n]
+    return False
