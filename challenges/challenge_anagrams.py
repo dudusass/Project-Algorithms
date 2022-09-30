d1d9681069fe73e2227026e69f802a7a45b8fbf3
Sass <dudu_sass12@hotmail.com>
2022-09-30 08:25:31 -0300

    feat:anagrams:

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    first_string = list(first_string.lower())
+    second_string = list(second_string.lower())
+    if len(first_string) != len(second_string):
+        return (False)
+    for i in first_string:
+        if i not in second_string:
+            return (False)
+        second_string.remove(i)
+    return (True)
