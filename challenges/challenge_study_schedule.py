c14758d4284eacdfdc93b352ad7aa3802a0cd876
Sass <dudu_sass12@hotmail.com>
2022-09-30 08:19:49 -0300

    feat:study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    try:
+        counter = 0
+        for i in range(0, len(permanence_period)):
+            if permanence_period[i][0] <= target_time <= permanence_period[i][1]:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
+
+
+
